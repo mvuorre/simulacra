962d42baeadff4a70ac7f48b9089e035d7ce7d24
Mvuorre <mv2521@columbia.edu>
2015-05-08 13:07:10 -0400

    Added K-S test to output

--- simulacra.R
+++ simulacra.R
@@ -32,11 +32,11 @@ subj <- readline(prompt="Subject ID: ")
 l1 = vector(mode="list", length=trials)
 l2 = vector(mode="list", length=trials)
 d <- data.frame(d=NA, d_obs=NA, cohens_d=NA, response=NA, resp=NA, 
-                p=NA, bf=NA, stringsAsFactors = FALSE)
+                p=NA, bf=NA, ksp=NA, stringsAsFactors = FALSE)
 
 # Loop over trials
 for (trial in 1:trials) {
-    d[trial,"d"] = rnorm(1) # Set difference in population means
+    d[trial,"d"] <- rnorm(1) # Set difference in population means
     l1[[trial]] = g1 = rnorm(n) # Sample for group 1
     l2[[trial]] = g2 = rnorm(n, mean = d[trial,"d"]) # Sample for group 2
     d[trial,"d_obs"] <- abs(mean(g1) - mean(g2)) # Observed difference in means
@@ -46,7 +46,9 @@ for (trial in 1:trials) {
     # Compute p-value from indp t-test
     d[trial, "p"] <- t.test(g1, g2)$p.val
     # Default bayes factor
-    d[trial, "bf"] = exp(ttestBF(g1, g2)@bayesFactor$bf)
+    d[trial, "bf"] <- ttestBF(g1, g2)@bayesFactor$bf
+    # Two-sample Kolmogorov-Smirnov test
+    d[trial, "ksp"] <- ks.test(g1, g2)$p.value
     # Create plot
     tmp = melt(data.frame(g1, g2), id.vars=NULL)
     print(ggplot(tmp, aes(variable, value)) + 
@@ -70,21 +72,26 @@ write.csv(d, paste(subj, ".csv", sep=""))
 m1 <- glm(resp ~ cohens_d, data=d, family='binomial')
 m2 <- glm(resp ~ p, data=d, family='binomial')
 m3 <- glm(resp ~ bf, data=d, family='binomial')
+m4 <- glm(resp ~ ksp, data=d, family='binomial')
 new1 <- data.frame(cohens_d = seq(min(d$cohens_d), 
                                   max(d$cohens_d), length=64))
 new2 <- data.frame(p = seq(min(d$p), max(d$p), length=64))
 new3 <- data.frame(bf = seq(min(d$bf), max(d$bf), length=64))
+new4 <- data.frame(ksp = seq(min(d$ksp), max(d$ksp), length=64))
 new1$fit_d <- predict(m1, type="response", newdata=new1)
 new2$fit_p <- predict(m2, type="response", newdata=new2)
 new3$fit_bf <- predict(m3, type="response", newdata=new3)
+new4$fit_ksp <- predict(m4, type="response", newdata=new4)
 m1_pse <- as.numeric(-coef(m1)[1]/coef(m1)[2])
 m2_pse <- as.numeric(-coef(m2)[1]/coef(m2)[2])
 m3_pse <- as.numeric(-coef(m3)[1]/coef(m3)[2])
+m4_pse <- as.numeric(-coef(m4)[1]/coef(m4)[2])
 
 # Plot subject's responses
 m1_x <- sort(c(seq(0, max(d$cohens_d), length=3), m1_pse))
 m2_x <- sort(seq(m2_pse, max(d$p), length=4))
 m3_x <- sort(seq(m3_pse, max(d$bf), length=4))
+m4_x <- sort(seq(m4_pse, max(d$ksp), length=4))
 # Cohen's d plot
 m1_plot <- ggplot(new1, aes(x=cohens_d, y=fit_d)) + 
     geom_vline(x=m1_pse, col="red", lty=2, size=1.2) +
@@ -105,7 +112,7 @@ m2_plot <- ggplot(new2, aes(x=p, y=fit_p)) +
     geom_line(size=1.2) +
     scale_x_continuous(breaks=m2_x) +
     labs(x="p-value", y="P(signal)", 
-         title="p-value")
+         title="t-test")
 # Bayes factor plot
 m3_plot <- ggplot(new3, aes(x=bf, y=fit_bf)) + 
     geom_vline(x=m3_pse, col="red", lty=2, size=1.2) +
@@ -113,14 +120,21 @@ m3_plot <- ggplot(new3, aes(x=bf, y=fit_bf)) +
     geom_point(data=d,aes(y=resp), shape=1, size=4) +
     geom_point(data=d,aes(y=resp), alpha=.4, size=4) +
     geom_line(size=1.2) +
-    scale_x_continuous(breaks=m3_x) +
-    labs(x="bayes factor", y="P(signal)", 
+    scale_x_continuous(breaks=c(m3_x)) +
+    #coord_cartesian(xlim=c(0,20)) +
+    labs(x="logBF", y="P(signal)", 
          title="Bayes factor")
-# Plot observed differences
-p_df <- ggplot(d, aes(x=d_obs)) + 
-    stat_bin(geom="histogram", col="white") +
-    labs(x="absolute difference", y="",
-         title="Differences in means")
+# Kolmogorov-Smirnov p-value plot
+m4_plot <- ggplot(new4, aes(x=ksp, y=fit_ksp)) + 
+    geom_vline(x=m4_pse, col="red", lty=2, size=1.2) +
+    geom_hline(y=.5, col="red", lty=3, size=.7) +
+    geom_point(data=d,aes(y=resp), shape=1, size=4) +
+    geom_point(data=d,aes(y=resp), alpha=.4, size=4) +
+    geom_line(size=1.2) +
+    scale_x_continuous(breaks=m4_x) +
+    labs(x="K-S p-value", y="P(signal)", 
+         title="Kolmogorov-Smirnov test")
+
 grid.arrange(ncol=2, m1_plot, m2_plot,
-             m3_plot, p_df)
+             m3_plot, m4_plot)
     
